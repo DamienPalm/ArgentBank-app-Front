e41e9779453743cfd7f74e8b4a74f3555ed10e14
Damien Palm <palm.damien.pro@gmail.com>
2025-04-17 16:03:30 +0200

    modification nom classe

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,13 +2,15 @@ import "../styles/Hero.css";
 
 export function Hero() {
   return (
-    <div className="hero">
-      <section className="hero__heroContent">
+    <div className="main__hero">
+      <section className="main__hero__heroContent">
         <h2 className="sr-only">Promoted Content</h2>
-        <p className="hero__heroContent__subtitle">No fees.</p>
-        <p className="hero__heroContent__subtitle">No minimum deposit.</p>
-        <p className="hero__heroContent__subtitle">High interest rates.</p>
-        <p className="hero__heroContent__text">
+        <p className="main__hero__heroContent__subtitle">No fees.</p>
+        <p className="main__hero__heroContent__subtitle">No minimum deposit.</p>
+        <p className="main__hero__heroContent__subtitle">
+          High interest rates.
+        </p>
+        <p className="main__hero__heroContent__text">
           Open a savings account with Argent Bank today!
         </p>
       </section>
